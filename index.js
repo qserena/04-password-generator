844cc77f0dfa0911a4d05b137efcde2529136937
Tom Serenander <tom@serenander.se>
2023-09-07 18:24:33 +0200

    First draft

---
+++ index.js
@@ -0,0 +1,113 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+]
+
+let passwordOneElem = document.getElementById("password-one")
+let passwordTwoElem = document.getElementById("password-two")
+
+console.log("Hej")
+
+function generatePassword(passwordLength) {
+  let password = ""
+  for (let i = 0; i < passwordLength; i++) {
+    let index = Math.floor(characters.length * Math.random())
+    password += characters[index]
+  }
+  console.log(password)
+  return password
+}
+
+function generatePasswords() {
+  passwordOneElem.textContent = generatePassword(15)
+  passwordTwoElem.textContent = generatePassword(15)
+}
